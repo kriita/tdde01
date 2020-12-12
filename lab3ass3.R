e4897e8f9c4f72cc2e92b0f5fa24814b64fce30c
Filip Jaredson <filja057@student.liu.se>
2020-12-12 21:35:24 +0100

    lab3 ass3 done

--- lab3ass3.R
+++ lab3ass3.R
@@ -12,7 +12,7 @@ te <- mydata[26:500,] # Test
 winit <- runif(1000,-1,1)
 nn <- neuralnet(Sin ~ Var, tr, hidden=c(8,5), startweights = winit, threshold = 0.0002)
 # Plot of the training data (black), test data (blue), and predictions (red)
-plot(tr, cex=2)
+plot(tr, main="Predicting sin(x), [0,10]", xlab="x", ylab='sin(x)', cex=2)
 points(te, col = "blue", cex=1)
 points(te[,1],predict(nn,te), col="red", cex=1)
 
@@ -22,16 +22,17 @@ Var <- runif(500, 0, 20)
 te2 <- data.frame(Var, Sin=sin(Var))
 
 # plot new data
-plot(te2[,1],predict(nn,te2), main="boop", col="red")
+plot(te2[,1],predict(nn,te2), main="Predicting sin(x), [0,20]", xlab="x", ylab='sin(x)', col="red")
 points(te2[,1], te2[,2], col = "blue", cex=1)
 
 # Predict sin
 set.seed(1234567890)
-X <- runif(500, 0, 20)
+X <- runif(500, 0, 10)
 tr2 <- data.frame(Sin=sin(X), X)
 
 # plot new data
-nn2 <- neuralnet(X ~ Sin, tr2, hidden=c(8,5), startweights = winit, threshold = 0.02)
+nn2 <- neuralnet(X ~ Sin, tr2, hidden=c(4,3,4), startweights = winit, threshold = 2)
 
-plot(te2[,1], predict(nn2,te2), main="boop2", col="red", ylim=c(-1.2,12))
-points(te2[,1], te2[,2], col="blue", cex=1)
+plot(tr2[,1], tr2[,2], main="Predicting x, [0,10]", xlab="sin(x)", ylab='x', col="blue", ylim=c(0,10))
+points(tr2[,1], predict(nn2,tr2), col="red", cex=1)
+
